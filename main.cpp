998e956e7a04970ab1c9bd250d0b1cf3628bf176
Ashwin Kishin Banwari <ashwinkbanwari@gmail.com>
2024-10-27 18:50:38 -0700

    create a blue empty window that stays on screen

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "SDL3/SDL.h"
 
 #include <iostream>
-#include <format>
 
 constexpr int SCREEN_WIDTH = 640;
 constexpr int SCREEN_HEIGHT = 480;
@@ -10,20 +9,52 @@ using namespace std;
 
 int main(int argc, char** args)
 {
+    SDL_Log("main():\n");
     SDL_Window* window = NULL;
     SDL_Surface* screenSurface = NULL;
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    if (!SDL_Init(SDL_INIT_VIDEO))
     {
-        cerr << format("SDL could not initialize! SDL_Error: {}\n", SDL_GetError());
+        SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     }
     else
     {
-        /*//Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+        if (window == NULL)
         {
-            cerr << format("Window could not be created! SDL_Error: {}\n", SDL_GetError());
-        }*/
+            SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            //Get window surface
+			screenSurface = SDL_GetWindowSurface(window);
+
+			//Fill the surface white
+			SDL_FillSurfaceRect(screenSurface, nullptr, SDL_MapSurfaceRGB(screenSurface, 0, 0, 0xFF));
+			
+			//Update the surface
+			SDL_UpdateWindowSurface(window);
+
+			//Hack to get window to stay up
+			SDL_Event e;
+            bool quit = false;
+            while (quit == false)
+            {
+                while(SDL_PollEvent(&e))
+                {
+                    if (e.type == SDL_EVENT_QUIT)
+                        quit = true;
+                }
+            }
+        }
     }
+
+    //Destroy window
+	SDL_DestroyWindow(window);
+
+	//Quit SDL subsystems
+	SDL_Quit();
+    
+    return 0;
 }
