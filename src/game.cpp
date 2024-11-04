c758e97d01bdf37826be3d68e68a1434f3d21bf5
Ashwin Kishin Banwari <ashwinkbanwari@gmail.com>
2024-11-03 22:50:53 -0800

    debugging black screen

--- src/game.cpp
+++ src/game.cpp
@@ -18,25 +18,19 @@ Game::Game()
         fatal("Window could not be created! SDL_Error: %s\n", SDL_GetError());
     }
 
-    // Get window surface
-    SDL_Surface* screenSurface = SDL_GetWindowSurface(m_window);
-
-    // Fill the surface white
-    SDL_FillSurfaceRect(screenSurface, nullptr, SDL_MapSurfaceRGB(screenSurface, 0x0, 0x0, 0x0));
-
-    // Update the surface
-    SDL_UpdateWindowSurface(m_window);
-
     // load opengl
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GLContext glContext = SDL_GL_CreateContext(m_window);
     if (glContext == NULL)
     {
         fatal("OpenGL context could not be created! SDL_Error: %s\n", SDL_GetError());
     }
-    SDL_GL_MakeCurrent(m_window, glContext);
+    if (!SDL_GL_MakeCurrent(m_window, glContext))
+    {
+        fatal("Failed to make current context! SDL_Error: %s\n", SDL_GetError());
+    }
     m_drawer = std::make_unique<Drawer>(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 
