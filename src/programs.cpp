83b4f0cc2fda404977cd5d5d2b7a964f22bc37ab
Ashwin banwari <ashwinkbanwari@gmail.com>
2025-05-17 00:06:46 -0700

    print func address

--- src/programs.cpp
+++ src/programs.cpp
@@ -25,6 +25,7 @@ unsigned int compileProgram(const char* vertexShaderSource, const char* fragment
     std::cout << "Vertex shader source: " << vertexShaderSource << std::endl;
     std::cout << "Fragment shader source: " << fragmentShaderSource << std::endl;
     std::cout << "huh" << std::endl;
+    std::cout << &glCreateShader << std::endl;
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     std::cout << "c" << std::endl;
     glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
