65a6395b99de98a8540bea466c8cf95f9c26693c
Ashwin Banwari <52060395+ashwinbanwari@users.noreply.github.com>
2025-05-17 02:16:11 -0700

    remove some prints

--- src/programs.cpp
+++ src/programs.cpp
@@ -24,17 +24,12 @@ unsigned int compileProgram(const char* vertexShaderSource, const char* fragment
     std::cout << "Beginning compilation of shader program" << std::endl;
     std::cout << "Vertex shader source: " << vertexShaderSource << std::endl;
     std::cout << "Fragment shader source: " << fragmentShaderSource << std::endl;
-    std::cout << "huh" << std::endl;
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
     std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
-    std::cout << &glCreateShader << std::endl;
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    std::cout << "c" << std::endl;
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
 
-    std::cout << "a" << std::endl;
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
-    std::cout << "b" << std::endl;
 
     int success;
     char infoLog[512];
