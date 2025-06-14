9b68506a60cefcd5e61d6086751dfba901518f97
Ashwin banwari <ashwinkbanwari@gmail.com>
2025-06-13 20:52:09 -0700

    works with import std;

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,6 +1,10 @@
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_STANDARD 23)
+set(CMAKE_EXPERIMENTAL_CXX_IMPORT_STD
+  # This specific value changes as experimental support evolves. See
+  # `Help/dev/experimental.rst` in the CMake source corresponding to
+  # your CMake build for the exact value to use.
+  "a9e1cf81-9932-4810-974b-6eccaf14e457")
 set(CMAKE_TOOLCHAIN_FILE $ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake CACHE FILEPATH "The cmake toolchain file")
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 string(REPLACE "\\" "/" CMAKE_TOOLCHAIN_FILE ${CMAKE_TOOLCHAIN_FILE})
+set(CMAKE_CXX_MODULE_STD 1)
