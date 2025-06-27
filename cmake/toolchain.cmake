74d391f472cf4bd23e63455912f312bbffbeed02
Ashwin Banwari <ashwinkbanwari@gmail.com>
2025-06-27 03:24:07 -0700

    update experimental hash for cmake 4.0.3 (works on Linux)

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -3,7 +3,7 @@ set(CMAKE_EXPERIMENTAL_CXX_IMPORT_STD
   # This specific value changes as experimental support evolves. See
   # `Help/dev/experimental.rst` in the CMake source corresponding to
   # your CMake build for the exact value to use.
-  "a9e1cf81-9932-4810-974b-6eccaf14e457")
+  "d0edc3af-4c50-42ea-a356-e2862fe7a444")
 set(CMAKE_TOOLCHAIN_FILE $ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake CACHE FILEPATH "The cmake toolchain file")
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 string(REPLACE "\\" "/" CMAKE_TOOLCHAIN_FILE ${CMAKE_TOOLCHAIN_FILE})
